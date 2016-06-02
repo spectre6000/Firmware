bdb65f31b6228d420ce93f3753f882174ca932ff
spectre6000 <spectre6000@gmail.com>
2016-06-02 10:31:08 -0600

    keymaps created and winnowed

---
+++ keyboard/prototype/keymap_prototype.c
@@ -0,0 +1,55 @@
+#include "keymap_common.h"
+
+/*
+ * Hasu
+ */
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Keymap 0: Numbers
+     * ,-----------.
+     * |  1|  2|  3|
+     * |-----------|
+     * |  4|  5|  6|
+     * |-----------|
+     * |  7|  8| Fn|
+     * `-----------'
+     */
+    KEYMAP_ANSI(
+        1,   2,   3, /
+        4,   5,   6, /
+        7,   8,   FN0),
+    /* Keymap 1: Letters
+     * ,-----------.
+     * |  A|  B|  C|
+     * |-----------|
+     * |  D|  E|  F|
+     * |-----------|
+     * |  G|  H| Fn|
+     * `-----------'
+     */
+    KEYMAP_ANSI(
+        A,   B,   C, \
+        D,   E,   F, \
+        G,   H,   FN1),
+    /* Keymap 2: Special Characters
+     * ,-----------.
+     * |  -|  =|  \|
+     * |-----------|
+     * |  [|  ]|del|
+     * |-----------|
+     * |  "|  ,| Fn|
+     * `-----------'
+     */
+    KEYMAP_ANSI(
+        MINS, EQL,BSLS, \
+        LBRC,RBRC,BSPC, \
+        QUOT,COMM, FN2),
+};
+
+/*
+ * Fn action definition
+ */
+const uint16_t PROGMEM fn_actions[] = {
+    [0] = ACTION_DEFAULT_LAYER_SET(1),  // set Numbers layout
+    [1] = ACTION_DEFAULT_LAYER_SET(2),  // set Letters layout
+    [2] = ACTION_DEFAULT_LAYER_SET(0),  // set Special Characters layout\
+};
